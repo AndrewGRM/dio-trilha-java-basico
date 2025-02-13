2f98d40781d8406b2aaa43f4bcbab308be29a13f
AndrewGRM <andrew.grm15@gmail.com>
2025-02-13 17:24:31 -0300

    Adicionando conteúdo de Estrutura de repetição em Java

---
+++ controle-fluxo/src/ExemploForArrays.java
@@ -0,0 +1,9 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+        String alunos [] = {"Renato", "Jean", "João", "Gustavo"};
+
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
