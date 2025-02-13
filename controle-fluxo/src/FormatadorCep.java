6b7f20fdd90659f41ba930ee49bdb250779f7696
AndrewGRM <andrew.grm15@gmail.com>
2025-02-13 20:24:18 -0300

    Adicionando conteúdos sobre Exceções de erros

---
+++ controle-fluxo/src/FormatadorCep.java
@@ -0,0 +1,19 @@
+public class FormatadorCep {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado); 
+        } catch (CepInvalidoException e) {
+            e.printStackTrace();
+            System.out.println("O cep não corresponde as regras de négocio");
+        }
+
+    }
+    static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8)
+            throw new CepInvalidoException();
+
+            // simulando um cep formatado
+            return "23.765-064";
+    }
+}
--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,10 +1,11 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     
     public static void main(String[] args) throws Exception {
-        
+        try {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Digite seu Nome:");
@@ -23,7 +24,9 @@ public class AboutMe {
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + " cm ");
-        
-        
+        }
+        catch (InputMismatchException e) {
+            System.err.println("Os campos 'Idade' e 'altura' precisam ser numéricos e em altura tem que ser '.'");
+        } 
     }   
 }
